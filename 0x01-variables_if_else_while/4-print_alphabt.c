404d4e8b79c1c716573f407ef7de98d9cc775e47
Munashe Matarutse <munashematarutse@gmail.com>
2023-06-01 15:17:12 -0700

    Update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lowercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+		{
+			putchar(alphabet);
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
+
+
